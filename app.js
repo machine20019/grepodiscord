ef017996b43fe857e2e7eade4fd785c8c79dfb36
Brian Tanner <brian.tanner@campingworld.com>
2016-03-20 15:18:02 -0400

    added express server

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 "use strict";
 
+const http = require('http');
 const path = require('path');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const logger = require('./lib/logger');
 
-let bot = require('./lib/bot');
+let bot = require('./lib/bot'),
+    app = express();
 
 require('dotenv').load();
 
@@ -22,3 +27,32 @@ let config = {
 
 // start bot
 bot(config);
+
+/**
+ * Start Web Server setup
+ */
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  partialsDir: 'views/partials/'
+}));
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'Grepolis Tools https://github.com/briantanner/Grepolis-Tools');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('This is the Grepolis Discord Bot. Nothing to show here.');
+});
+
+// create server
+http.createServer(app).listen(app.get('port'), () => {
+  logger.info('Express server listening on port %d', app.get('port'));
+});
